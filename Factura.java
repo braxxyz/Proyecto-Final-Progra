a1cbfde7f651b2579d1d804069b46aa095171c7e
braxxyz <yenamoretty2327@gmail.com>
2025-11-05 21:20:31 -0600

    clase facttura no terminada

---
+++ Factura.java
@@ -0,0 +1,24 @@
+public class Factura {
+
+private  Pasajero pasajero;
+private  Vuelo vuelo;
+private  String  clase;
+private double monto;
+
+public Factura (Pasajero pasajero, Vuelo vuelo, String clase, double monto) {
+    this.pasajero = pasajero;
+    this.vuelo = vuelo;
+    this.clase = clase;
+    this.monto = monto; 
+}
+
+public String GenerarTextoFactura(){
+
+}
+
+
+
+
+
+
+}
